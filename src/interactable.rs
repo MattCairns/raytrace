06064c3023291f75b1064173956fe1e97af05e40
Matthew Cairns <mattrcairns@gmail.com>
2022-01-05 18:18:19 -0800

    Added hittablelist

--- src/interactable.rs
+++ src/interactable.rs
@@ -1,10 +1,43 @@
 use crate::ray::Ray;
 use crate::vec3::Vec3;
 
+pub struct HittableList {
+    pub hittables: Vec<Sphere>,
+}
+
+impl HittableList {
+    pub fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &HitRecord) -> bool {
+        let mut temp_rec;
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for ob in &self.hittables {
+            if ob.hit(r, t_min, closest_so_far, temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = &temp_rec;
+            }
+        }
+        hit_anything
+    }
+}
+
 pub struct HitRecord {
     pub p: Vec3,
     pub norm: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&self, r: &Ray, outward_norm: &Vec3) {
+        self.front_face = r.direction.dot(outward_norm) < 0.0;
+        if self.front_face {
+            self.norm = *outward_norm
+        } else {
+            self.norm = -*outward_norm
+        };
+    }
 }
 
 pub struct Sphere {
@@ -13,7 +46,7 @@ pub struct Sphere {
 }
 
 impl Sphere {
-    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, mut rec: HitRecord) -> bool {
+    pub fn hit(&self, r: &Ray, t_min: f64, t_max: f64, mut rec: HitRecord) -> bool {
         let oc = r.origin - self.center;
         let a = r.direction.dot(&r.direction);
         let b = 2.0 * oc.dot(&r.direction);
@@ -34,6 +67,8 @@ impl Sphere {
                 rec.t = rootb
             }
             rec.p = r.at(rec.t);
+            let outward_norm = (rec.p - self.center) / self.radius;
+            rec.set_face_normal(r, &outward_norm);
             rec.norm = (rec.p - self.center) / self.radius;
             true
         }
